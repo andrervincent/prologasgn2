7e6fdff4abd760e277254b8619096343d1574f8b
André Vincent <andrevncnt7@gmail.com>
2019-02-22 12:52:49 -0500

    Corrects comments of general sum and nested list rule.

--- prolog.pl
+++ prolog.pl
@@ -23,7 +23,6 @@ sum-up-numbers-simple([FirstNum | RestofList],Sum) :-
 %
 %
 %	Part 2. sum-up-numbers-general
-
 % Check base case - list is/is not empty.
 sum-up-numbers-general([], 0).
 % Second case - if list is not empty, and 
@@ -31,18 +30,20 @@ sum-up-numbers-general([], 0).
 % first element + rest of (current) list.
 sum-up-numbers-general([FirstNum | RestofList], Sum) :-
 	number(FirstNum),
-	sum-up-numbers-simple(RestofList, RestResult),
+	sum-up-numbers-general(RestofList, RestResult),
 	Sum is FirstNum + RestResult.
+% if the current element is not a number, 
+% but it is a list, calculate the result of that list.
+
+sum-up-numbers-general([FirstElem | RestofList],Sum) :-
+    is_list(FirstElem),
+    is_list(RestofList),
+    sum-up-numbers-general(FirstElem, FirstResult).
+    
 % if the current element is not a number,
 % use the aforementioned rule on the rest
 % of the list to calculate the subsequent 
-% sum.
+% sum.    
 sum-up-numbers-general([FirstNum | RestofList],Sum) :-
     not(number(FirstNum)),
-    is_list(FirstNum),
-    sum-up-numbers-simple(RestofList, Sum).
-    
-sum-up-numbers-general([FirstNum | RestofList],Sum) :-
-    not(number(FirstNum)),
-    sum-up-numbers-simple(RestofList, Sum).
-
+    sum-up-numbers-general(RestofList, Sum).
