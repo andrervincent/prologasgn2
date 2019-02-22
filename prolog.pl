08ad4314bdd05eb67d4c3e9b789163e3e3ce0558
André Vincent <andrevncnt7@gmail.com>
2019-02-22 00:13:59 -0500

    First function: finishes third rule of sum general list portion

--- prolog.pl
+++ prolog.pl
@@ -1,10 +1,17 @@
-% Check base case - list is/is not empty.
+% Check base case - list is/is not emptRestofList.
 sum-up-numbers-simple([], 0).
-% Second case - if list is not empty, and 
+% Second case - if list is not emptRestofList, and 
 % the first element is a number, compute:
 % first element + rest of (current) list.
-sum-up-numbers-simple([First | Rest], N) :-
-	numbers(First),
-	sum-up-numbers-simple(Rest, RestResult),
-	Result is First + Result.
+sum-up-numbers-simple([FirstNum | RestofList], Sum) :-
+	number(FirstNum),
+	sum-up-numbers-simple(RestofList, RestResult),
+	Sum is FirstNum + RestResult.
+% if the current element is not a number,
+% use the aforementioned rule on the rest
+% of the list to calculate the subsequent 
+% sum.
+sum-up-numbers-simple([FirstNum | RestofList],Sum) :-
+    not(number(FirstNum)),
+    sum-up-numbers-simple(RestofList, Sum).
 
